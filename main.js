ba02ab1099a2b7a6c4e8d8d82a6bf84615c8cfb4
olegdrobot <40525287+olegdrobot@users.noreply.github.com>
2021-05-09 22:01:56 +0300

    delete comments

--- main.js
+++ main.js
@@ -7,17 +7,14 @@ exports.encode = (key) =>{
 		const charArr = [...text];
 		let fKey = key;
 		if(key >26) fKey = key%26;
-	//console.log('key ', key);
 		const codeArr = charArr.map((el)=>{
 		let codeElement = el.charCodeAt();
-		//console.log('codeElement ', codeElement);
 		if(codeElement >= 65 && codeElement<=90){
 			codeElement += fKey;
 			if(codeElement > 90) codeElement = 64 + (codeElement - 90);
 		}
 		if(codeElement >= 97 && codeElement <= 122){
 			codeElement += fKey;
-			//console.log('codeElement after ', codeElement);
 			if(codeElement > 122) codeElement = 96 + (codeElement - 122); 
 		}
 		
@@ -52,63 +49,3 @@ exports.decode = (key) =>{
 	callback();
 	}
 }
-
-/*
-function encrypt(text, key){
-	const charArr = [...text];
-	const codeArr = charArr.map((el)=>{
-		let codeElement = el.charCodeAt();
-		if(codeElement >= 65 && codeElement<=90){
-			codeElement += key;
-			if(codeElement > 90) codeElement = 64 + (codeElement - 90);
-		}
-		if(codeElement >= 97 && codeElement <= 122){
-			codeElement += key;
-			if(codeElement > 122) codeElement = 96 + (codeElement - 122); 
-		}
-		
-		return codeElement;
-	});
-
-	return codeArr.map((el)=>String.fromCharCode(el)).join('');
-}
-
-function decrypt(text, key){
-	const charArr = [...text];
-	const codeArr = charArr.map((el)=>{
-		let codeElement = el.charCodeAt();
-		//console.log('codeElement ', codeElement);
-		if(codeElement >= 65 && codeElement<=90){
-			codeElement -= key;
-			if(codeElement < 65) codeElement = 91 - (65 - codeElement);
-		}
-		if(codeElement >= 97 && codeElement <= 122){
-			codeElement -= key;
-			//console.log('codeElement after ', codeElement);
-			if(codeElement < 97) codeElement = 123 - (97 - codeElement); 
-		}
-		
-		return codeElement;
-	});
-	return codeArr.map((el)=>String.fromCharCode(el)).join('');
-}
-
-exports.writeFile=(method, key, input, output)=>{
-	console.log('method: ', method, 'key: ', key, 'input: ', input, "output: ", output);
-	fs.readFile(input, function (err, data) {
-   		let str='';
-   		if (err) {
-     		 return console.log('ERROR: Invalid file path!' + '\n');
-   		}
-   		if(method == 'encrypt') str = encrypt(data.toString(), key);
-   		if(method == 'decrypt') str = decrypt(data.toString(), key);
-   		console.log("encrypt: " + str);
-   		fs.writeFile(output, str, function(err){
-   			 if (err) {
-     			 return console.error(err);
-   			 }
-   		});
-	});
-}
-
-*/
